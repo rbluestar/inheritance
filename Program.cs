84820db0b7aecb092754a181b9d7520ebc2431f1
Roja RK <roja.ramanagouda@kalpitatechnologies.com>
2022-08-04 16:12:51 +0530

    First comit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Application
+{
+    //String gallery;
+    //String contacts;
+    //string files;
+        public void Message()
+    {
+        Console.WriteLine(" message");
+    }
+    public void call()
+    {
+        Console.WriteLine(" calling");
+    }
+    public void Share()
+    {
+        Console.WriteLine("share documents");
+    }
+    class wattsup : Application
+    {
+        public void MessageWattsup()
+        {
+            Console.WriteLine("Wattsup running");
+        }
+    }
+}
+ static void main(string args) {
+    Wattsup w = new Wattsup();
+    w.Message();
+}
