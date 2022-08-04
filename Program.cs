b3b3b75bba8e18c2e919058b94b2e2e52b53f2a6
Roja RK <roja.ramanagouda@kalpitatechnologies.com>
2022-08-04 17:22:35 +0530

    first comit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Debitcard
+{
+    public void Withdraw()
+    {
+        Console.WriteLine("Card is inserted.");
+    }
+}
+class SbiCard : Debitcard
+{
+    public void WithdrawOne()
+    {
+        Console.WriteLine("SBI card inserted...");
+    }
+    public void MoneyTransfer()
+    {
+        Console.WriteLine("SBI money transfering...");
+    }
+}
+
+class IcicCard : SbiCard
+{
+    public void WithdrawTwo()
+    {
+        Console.WriteLine("Icic Debit card");
+    }
+    static void Main(string[] args)
+    {
+       IcicCard s = new IcicCard();
+        s.Withdraw();
+        s.WithdrawOne();
+       
+        s.WithdrawTwo();
+        s.MoneyTransfer();
+        Console.Read();
+    }
+}
